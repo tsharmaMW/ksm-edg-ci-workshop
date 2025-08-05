a2af0f37ac69ba15aeafa7e7e181dd73fc8fc9c3
tsharmaMW <tsharma@mathworks.com>
2025-08-05 10:37:40 +0530

    add failing tests

---
+++ tests/dummyTestCases.m
@@ -0,0 +1,19 @@
+classdef dummyTestCases < matlab.unittest.TestCase
+    methods(Test)
+        function failingTestVerify(testCase)
+            actSolution = [7.5, 10, 46];
+            expSolution = [7.5, 10, 45];
+            testCase.verifyEqual(actSolution,expSolution)
+        end
+        function validateCalculationResult(testCase)
+            actSolution = [7.5, 10, 45];
+            expSolution = [7.5, 10, 45];
+            testCase.verifyEqual(actSolution,expSolution)
+        end
+        function fail(testCase)
+            actSolution = [46];
+            expSolution = [7.5, 10, 45];
+            testCase.verifyEqual(actSolution,expSolution)
+        end
+    end
+end
